6f57cf07a189521b386eb6d6f95678bbc704b12e
MuthuCM <45748076+MuthuCM@users.noreply.github.com>
2025-08-24 23:11:07 +0530

    Update example2.py

--- Chapter_6/example2.py
+++ Chapter_6/example2.py
@@ -14,7 +14,7 @@ cr = classifier.fit(X,y)
 # Calculate Accuracy
 from sklearn import metrics
 y_pred = classifier.predict(X)
-print (metrics.fl_score(y, y_pred, average='weighted'))
+print (metrics.f1_score(y, y_pred, average='weighted'))
 
 # Visualize the Line of Seperation
 mglearn.plots.plot_2d_separator(cr, X, fill = False, eps = 0.5)
@@ -28,4 +28,5 @@ mglearn.discrete_scatter(X[:, 0], X[:, 1], y)
 plt.title("KNN Classifier")
 plt.xlabel("Feature 0")
 plt.ylabel("Feature 1")
+
 plt.show()
