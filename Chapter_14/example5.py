42b3b0813d3e163823d455c245a7f88e994cf018
MuthuCM <45748076+MuthuCM@users.noreply.github.com>
2025-08-25 22:46:02 +0530

    Update example5.py

--- Chapter_14/example5.py
+++ Chapter_14/example5.py
@@ -3,12 +3,12 @@
 # Import the libraries
 import numpy as np
 import pandas as pd
-import matplotlib.pyplotas plt
+import matplotlib.pyplot as plt
 
 df = pd.read_csv ("Mall_Customers.csv")
 X = df.iloc[:, [3,4]].values
-from sklearn.preprocessing import StandardScalar
-sc = StandardScalar( )
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler( )
 X = sc.fit_transform(X)
 
 import scipy.cluster.hierarchy as sch
@@ -16,4 +16,5 @@ dendrogram =sch.dendrogram (sch.linkage (X, method='ward'))
 plt.title ('Dendrogram')
 plt.xlabel ('Customers')
 plt.ylabel ('Euclidean Distances')
+
 plt.show ( )
