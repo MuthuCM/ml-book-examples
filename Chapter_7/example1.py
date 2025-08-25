9551da0ff4d725f4277184a8baf4884b77add8ed
MuthuCM <45748076+MuthuCM@users.noreply.github.com>
2025-08-25 06:46:38 +0530

    Update example1.py

--- Chapter_7/example1.py
+++ Chapter_7/example1.py
@@ -19,21 +19,22 @@ Y = df['RiskLevel'].values
 # Do Scaling of Data
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler( )
-X = sc.fit_transform (X_train)
+X = sc.fit_transform (X)
 
 # Fit KNN Classifier Model
 from sklearn.svm import SVC
-classifier = SVC (Kernel='linear', random-state=0)
-classifier.fit (X,y)
+classifier = SVC (kernel='linear', random_state=0)
+classifier.fit (X,Y)
 
 # Calculate Accuracy
 from sklearn import metrics
 y_predict = classifier.predict(X)
-print ("F-Score: ", metrics.fl_score(y, y_predict, average = 'weighted'))
+print ("F-Score: ", metrics.fl_score(Y, y_predict, average = 'weighted'))
 
 # Do Prediction
 input_data = np.array ([[35,120,60,6.1,98.0,76]])
 input_data = input_data.reshape (len(input_data), -1)
 input_data  = sc.fit_transform(input_data )
 predicted_value = classifier.predict (input_data)
+
 print (predicted_value)
