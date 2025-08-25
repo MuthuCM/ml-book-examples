ef7b41b7b99edc1a3da8c49eea1dcf627cf5d6b1
MuthuCM <45748076+MuthuCM@users.noreply.github.com>
2025-08-25 19:12:45 +0530

    Update example6.py

--- Chapter_9/example6.py
+++ Chapter_9/example6.py
@@ -22,5 +22,38 @@ for col in categorical_columns:
 target = data['Yield']
       features=data[['Crop','Area','Production','Annual_Rainfall',                               
                                               'Fertilizer', 'Pesticide' ]]       
+# Split into Training and Testing Sets
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split (features, target,  
+                                     test_size=0.3, random_state = 42)
+
+# Do Scaling of Data
+scaler = standardScaler()
+X_train_scaled = scaler.fit_transform(x_train)
+X_test_scaled = scaler.transform(x_test)
+# Fit AdaBoost Regressor Model
+from sklearn.ensemble import AdaBoostRegressor
+ab_model = AdaBoostRegressor(random_state=42 )
+ab_model.fit(X_train_scaled,y_train)
+
+# Do prediction with AdaBoost Regressor Model
+from sklearn.metrics import mean_squared_error, r2_score
+y_pred_ab = ab_model.predict(X_test_scaled)
+rmse_ab = mean_squared_error(y_test, y_pred_ab, squared=False)
+r2_ab = r2_score(y_test, y_pred_ab)
+print(f'AdaBoost - RMSE: {rmse_ab}, R²: {r2_ab}')
+# Fit GradientBoosting Regressor Model
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.ensemble import GradientBoostingRegressor
+gb_model = GradientBoostingRegressor(random_state=42 )
+gb_model.fit(X_train_scaled,y_train)
+
+# Do prediction with GradientBoosting Regressor Model
+from sklearn.metrics import mean_squared_error, r2_score
+y_pred_gb = gb_model.predict(X_test_scaled)
+rmse_gb = mean_squared_error(y_test, y_pred_gb, squared=False)
+r2_gb = r2_score(y_test, y_pred_gb)
+print(f'Gradient Boosting - RMSE: {rmse_gb}, R²: {r2_gb}')
+
 
 
