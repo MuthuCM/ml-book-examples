8b1cd95531907236aa05615639fbfe3f7dcbe38c
MuthuCM <45748076+MuthuCM@users.noreply.github.com>
2025-08-25 07:46:18 +0530

    Update example1.py

--- Chapter_9/example1.py
+++ Chapter_9/example1.py
@@ -5,16 +5,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load Data
-names = ['id', 'clump_thickness', 'uniform_cell_size', 
-	'uniform_cell_shape', 'marginal_adhesion', 
-	'signle_epithelial_size', 'bare_nuclei', 'bland_chromatin', 
-	'normal_nucleoli', 'mitoses', 'class']
-df = pd.read_table ('breast_cancer_wisconsin.data', sep=',', 
+names = ['id', 'clump_thickness', 'uniform_cell_size',
+        'uniform_cell_shape', 'marginal_adhesion',
+        'signle_epithelial_size', 'bare_nuclei', 'bland_chromatin',
+        'normal_nucleoli', 'mitoses', 'class']
+df = pd.read_table ('breast-cancer-wisconsin.data', sep=',',
                                                        names=names)
 
 # Preprocess the Data
-df.replace ('?', -99999, implace=True)
-df.drop (['id'], 1, inplace = True)
+df.replace ('?', -99999, inplace=True)
+df.drop (['id'], axis=1, inplace = True)
 
 # Define Independent and Dependent Variables
 X = df.iloc [:, [0,1,2,3,4,5,6,7,8]].values
@@ -22,11 +22,11 @@ Y = df.iloc [:, 9].values
 
 # Split into Training and Testing Sets
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25)
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.25)
 
 # Do Scaling of Data
-from sklearn.preprocessing import StandardScalar
-sc = StandardScalar( )
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler( )
 X_train = sc.fit_transform (x_train)
 X_test = sc.transform (x_test)
 
@@ -38,11 +38,12 @@ classifier.fit (X_train,y_train)
 
 # Calculate Accuracy
 from sklearn.metrics import accuracy_score
-y_pred = classifier.predct(X_test)
+y_pred = classifier.predict(X_test)
 print(f"Accuracy Score is: {accuracy_score(y_test, y_pred)}")
 
 # Do prediction with Random Forest Classifier Model
 input_data = np.array ([[4,2,1,1,1,2,3,2,1]])
-input_data = input_data.reshape(len(input_data),–1)
+input_data = input_data.reshape(len(input_data),-1)
 predicted_value = classifier.predict(input_data)
+
 print (f"Predicted Value is: {predicted_value}")
