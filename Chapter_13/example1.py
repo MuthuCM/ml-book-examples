46a8b4156d86135a2b50938cc2f6e84fa65a6c87
MuthuCM <45748076+MuthuCM@users.noreply.github.com>
2025-08-26 21:42:38 +0530

    Update example1.py

--- Chapter_13/example1.py
+++ Chapter_13/example1.py
@@ -1,139 +1,260 @@
 # Example 13.1
-# Load Packages
-# load dataset
-dataset = pd.read_csv('LoansData.csv.gz', compression='gzip', 
-                                                low_memory=True)
-#dataset.shape
-dataset[ 'loan_status' ].value_counts(dropna=False)
-dataset = dataset.loc[dataset [ 'loan_status' ].isin([ 'Fully Paid',  
-                                                    'Charged Off' ]) ]
-dataset[ 'loan_status' ].value_counts(normalize=True, dropna=False)
+# Load libraries
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot
+from pandas import read_csv, set_option
+from pandas.plotting import scatter_matrix
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split, KFold, cross_val_score, GridSearchCV
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import AdaBoostClassifier, GradientBoostingClassifier, RandomForestClassifier, ExtraTreesClassifier
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+#Libraries for Deep Learning Models
+from keras.models import Sequential
+from keras.layers import Dense
+#from keras.wrappers.scikit_learn import KerasClassifier
+from keras.optimizers import SGD
 
-dataset[ 'charged_off' ] = (dataset[ 'loan_status' ] ==
-                                'Charged Off').apply(np.uint8)
+#Libraries for Saving the Model
+from pickle import dump
+from pickle import load
+
+loans = pd.read_csv('LoansData_sample.csv.gz', compression='gzip', encoding='utf-8')
+dataset = loans
+
+#Diable the warnings
+import warnings
+warnings.filterwarnings('ignore')
+
+#type(dataset)
+# We're going to try to predict the loan_status variable. What are the value counts for this variable
+dataset['loan_status'].value_counts(dropna=False)
+
+dataset = dataset.loc[dataset['loan_status'].isin(['Fully Paid', 'Charged Off'])]
+dataset['loan_status'].value_counts(dropna=False)
+
+dataset['loan_status'].value_counts(normalize=True, dropna=False)
+
+dataset['charged_off'] = (dataset['loan_status'] == 'Charged Off').apply(np.uint8)
 dataset.drop('loan_status', axis=1, inplace=True)
 
-missing_fractions = dataset.isnull().mean( ).sort_values(ascending=False)
-drop_list=sorted(list(missing_fractions[missing_fractions> 0.3].index))
+missing_fractions = dataset.isnull().mean().sort_values(ascending=False)
+
+missing_fractions.head(10)
+
+drop_list = sorted(list(missing_fractions[missing_fractions > 0.3].index))
+print(drop_list)
+
+len(drop_list)
 dataset.drop(labels=drop_list, axis=1, inplace=True)
 dataset.shape
 
-keep_list = [ 'charged_off', 'funded_amnt', 'addr_state', 'annual_inc',    
-             'application_type', 'dti', 'earliest_cr_line',   
-             'emp_length', 'emp_title', 'fico_range_high', 
-             'fico_range_low','grade','home_ownership','id',        
-             'initial_list_status', 'installment',
-             'int_rate', 'loan_amnt', 'loan_status', 'mort_acc',  
-             'open_acc', 'pub_rec', 'pub_rec_bankruptcies', 
-             'purpose', 'revol_bal', 'revol_util', 
-             'sub_grade', 'term', 'title', 'total_acc', 
-             'verification_status', 'zip_code', 'last_pymnt_amnt', 
-             'num_actv_rev_tl', 'mo_sin_rcnt_rev_tl_op', 
-             'mo_sin_old_rev_tl_op', "bc_util", "bc_open_to_buy", 
-             "avg_cur_bal", "acc_open_past_24mths" ]
+print(sorted(dataset.columns))
+
+keep_list = ['charged_off','funded_amnt','addr_state', 'annual_inc', 'application_type', 'dti', 'earliest_cr_line', 'emp_length', 'emp_title', 'fico_range_high', 'fico_range_low', 'grade', 'home_ownership', 'id', 'initial_list_status', 'installment', 'int_rate', 'loan_amnt', 'loan_status', 'mort_acc', 'open_acc', 'pub_rec', 'pub_rec_bankruptcies', 'purpose', 'revol_bal', 'revol_util', 'sub_grade', 'term', 'title', 'total_acc', 'verification_status', 'zip_code','last_pymnt_amnt','num_actv_rev_tl', 'mo_sin_rcnt_rev_tl_op','mo_sin_old_rev_tl_op',"bc_util","bc_open_to_buy","avg_cur_bal","acc_open_past_24mths" ]
+
+len(keep_list)
+
 drop_list = [col for col in dataset.columns if col not in keep_list]
+
 dataset.drop(labels=drop_list, axis=1, inplace=True)
+
 dataset.shape
+# Identify and handle non-numeric columns for correlation calculation
+# The 'term' column is causing issues because it contains strings like ' 60 months'
+# Convert 'term' to numeric (months)
+dataset['term_months'] = dataset['term'].apply(lambda s: int(s.split()[0]))
 
-correlation = dataset.corr( )
-correlation_chargeOff = abs(correlation[ 'charged_off' ])
-drop_list_corr = sorted (list[correlation_chargeOff < 0.03] . index) 
-print( drop_list_corr)
+# Identify non-numeric columns
+non_numeric_columns = dataset.select_dtypes(exclude=['number']).columns
 
-dataset.drop(labels=drop_list_corr, axis=1, inplace=True)
-dataset[ [ 'id', 'emp_title', 'title', 'zip_code' ] ] .describe( )
-dataset.drop([ 'id', 'employment', 'title', 'zip_code' ], axis=1,            
-                                                         inplace=True)
+# Drop or convert non-numeric columns before calculating correlation
+# For simplicity, we will drop them for now. You might want to encode them for modeling later.
+dataset_numeric = dataset.drop(columns=non_numeric_columns)
 
-dataset[ 'term' ] = dataset[ 'term' ].apply(lambda s: 
-                                        np.int8(s.split( )[0] ) )
-dataset.groupby('term')['charged_off'].value_counts(normalize=True).loc[ :,1 ]
 
-dataset['emp_length'].replace(to_replace='10+ years', 
-                              value= '10 years', inplace=True )
-	
-dataset['emp_length'].replace('< 1 year' , '0 years', inplace=True )
-def emp_length_to_int( s ) :
-	if pd.isnull( s ) : 
-		return s
-	else:
-		return np.int8( s.split( ) [ 0 ] )
-dataset['emp_length'] = dataset[ 'emp_length' ].apply(emp_length_to_int)
-charge_off_rates=dataset.groupby('emp_length')['charged_off'].value_counts(normalize=True).loc[ :,1 ]
-sns.barplot(x=charge_off_rates.index, y=charge_off_rates.values )
+correlation = dataset_numeric.corr()
+correlation_chargeOff = abs(correlation['charged_off'])
+
+correlation_chargeOff.sort_values(ascending=False)
+
+drop_list_corr = sorted(list(correlation_chargeOff[correlation_chargeOff < 0.03].index))
+print(drop_list_corr)
+#Descriptive Statistics
+dataset.describe()
+#Ids are all unique and there are too many job titles and titles and zipcode,
+#these column is dropped The ID is not useful for modeling.
+dataset.drop(['emp_title','title','zip_code'], axis=1, inplace=True)
+
+#Data Dictionary: "The number of payments on the loan. Values are in months and can be either 36 or 60.".
+#The 60 Months loans are more likelely to charge off
+#Convert term to integers
+dataset['term'] = dataset['term'].apply(lambda s: np.int8(s.split()[0]))
+
+dataset.groupby('term')['charged_off'].value_counts(normalize=True).loc[:,1]
+
+dataset['emp_length'].replace(to_replace='10+ years', value='10 years', inplace=True)
+
+dataset['emp_length'].replace('< 1 year', '0 years', inplace=True)
+
+def emp_length_to_int(s):
+    if pd.isnull(s):
+        return s
+    else:
+        return np.int8(s.split()[0])
+
+dataset['emp_length'] = dataset['emp_length'].apply(emp_length_to_int)
+charge_off_rates = dataset.groupby('emp_length')['charged_off'].value_counts(normalize=True).loc[:,1]
+sns.barplot(x=charge_off_rates.index, y=charge_off_rates.values, color='#5975A4', saturation=1)
 
 dataset.drop(['emp_length'], axis=1, inplace=True)
-charge_off_rates = dataset.groupby('sub_grade')['charged_off'].value_counts(normalize=True).loc[ :,1 ]                                                                                              
-sns.barplot( x=charge_off_rates.index, y=charge_off_rates.values )
 
-dataset[ [ 'annual_inc' ] ] .describe( )
-dataset['log_annual_inc']=dataset['annual_inc'].apply(lambda x: np.log10(x+1) )
-dataset.drop( 'annual_inc', axis=1, inplace=True )
+charge_off_rates = dataset.groupby('sub_grade')['charged_off'].value_counts(normalize=True).loc[:,1]
+sns.set(rc={'figure.figsize':(12,5)})
+sns.barplot(x=charge_off_rates.index, y=charge_off_rates.values, color='#5975A4', saturation=1)
+dataset['earliest_cr_line'] = dataset['earliest_cr_line'].apply(lambda s: int(s[-4:]))
 
-dataset[ [ 'fico_range_low', 'fico_range_high'] ].corr( )
-dataset[ 'fico_score' ] = 0.5*dataset[ 'fico_range_low' ] + 0.5*dataset[ 'fico_range_high' ]
-dataset.drop(['fico_range_high, fico_range_low'],axis=1,inplace=True )
+dataset[['annual_inc']].describe()
 
-From import LabelEncoder
-# Create Categorical boolean mask
+dataset['log_annual_inc'] = dataset['annual_inc'].apply(lambda x: np.log10(x+1))
+dataset.drop('annual_inc', axis=1, inplace=True)
+
+dataset[['fico_range_low','fico_range_high']].corr()
+
+dataset['fico_score'] = 0.5*dataset['fico_range_low'] + 0.5*dataset['fico_range_high']
+
+dataset.drop(['fico_range_high', 'fico_range_low'], axis=1, inplace=True)
+
+dataset['charged_off'].value_counts()
+# Categorical boolean mask
 categorical_feature_mask = dataset.dtypes==object
-# Filter categorical columns using mask and turn it into a list
+# filter categorical columns using mask and turn it into a list
 categorical_cols = dataset.columns[categorical_feature_mask].tolist()
 
-loanstatus_0 = dataset[dataset[ "charged_off"]==0 ]
-loanstatus_1 = dataset[dataset[ "charged_off"]==1 ]
-subset_of_loanstatus_0 = loanstatus_0.smple(n=5500)
-subset_of_loanstatus_1 = loanstatus_1.smple(n=5500)
-dataset = pd.concat([subset_of_loanstatus_1, subset_of_loanstatus_0 ])
-dataset = dataset.sample(frac=1).reset_index(drop=True)
-print( "Current shape of dataset :", dataset.shape )
+categorical_cols
 
-Y = dataset[ "charged_off" ]
-X = dataset.loc[ :, dataset.columns != 'charged_off' ]
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+# apply le on categorical feature columns
+dataset[categorical_cols] = dataset[categorical_cols].apply(lambda col: le.fit_transform(col))
+dataset[categorical_cols].head(10)
+
+dataset.head(5)
+
+dataset.shape
+
+loanstatus_0 = dataset[dataset["charged_off"]==0]
+loanstatus_1 = dataset[dataset["charged_off"]==1]
+subset_of_loanstatus_0 = loanstatus_0.sample(n=5500)
+subset_of_loanstatus_1 = loanstatus_1.sample(n=5500)
+dataset = pd.concat([subset_of_loanstatus_1, subset_of_loanstatus_0])
+dataset = dataset.sample(frac=1).reset_index(drop=True)
+print("Current shape of dataset :",dataset.shape)
+dataset.head()
+
+#Filling the NAs with the mean of the column.
+dataset.fillna(dataset.mean(),inplace = True)
+# split out validation dataset for the end
+Y= dataset["charged_off"]
+X = dataset.loc[:, dataset.columns != 'charged_off']
 validation_size = 0.2
 seed = 7
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y, test_size=validation_size, random_state=seed)
 
-X_train, X_validation, Y_train, Y_validation = train_test_split( X, Y, test_size=validation_size, random_state=seed)
+dataset['charged_off'].value_counts()
+
+# test options for classification
 num_folds = 10
+seed = 7
+#scoring = 'accuracy'
+#scoring ='precision'
+#scoring ='recall'
 scoring = 'roc_auc'
-
-models = [ ]
-models.append( ( 'LR', LogisticRegression( ) ) )  
-models.append( ( 'LDA', LinearDiscriminantAnalysis( ) ) )  
-models.append( ( 'KNN', KNeighborsClassifier( ) ) )
-models.append( ( 'CART', DecisionTreeClassifier( ) ) )  
-models.append( ( 'NB'. GaussianNB( ) ) )
-# Neural Network
-models.append( ( 'NN', MLPClassifier ( ) ) )  
-# Ensemble Models
-# Boosting Methods
-models.append( ( 'AB', AdaBoostClassifier( ) ) )  
-models.append( ( 'GBM', GradientBoostingClassifier( ) ) )  
+# spot check the algorithms
+models = []
+models.append(('LR', LogisticRegression()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+#Neural Network
+models.append(('NN', MLPClassifier()))
+#Ensable Models
+# Boosting methods
+models.append(('AB', AdaBoostClassifier()))
+models.append(('GBM', GradientBoostingClassifier()))
 # Bagging methods
-models.append( ( 'RF', RandomForestClassifier( ) ) )  
-models.append( ( 'ET', ExtraTreesClassifier( ) ) )
-
+models.append(('RF', RandomForestClassifier()))
+models.append(('ET', ExtraTreesClassifier()))
+results = []
+names = []
+for name, model in models:
+    kfold = KFold(n_splits=num_folds, random_state=seed, shuffle=True)
+    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+	# compare algorithms
+fig = pyplot.figure()
+fig.suptitle('Algorithm Comparison')
+ax = fig.add_subplot(111)
+pyplot.boxplot(results)
+ax.set_xticklabels(names)
+fig.set_size_inches(15,8)
+pyplot.show()
 # Grid Search: GradientBoosting Tuning
-n_estimators = [20, 180]
-max_depth= [3, 5]
-param_grid = dict(n_estimators=n_estimators, max_depth=max_depth)
-model = GradientBoostingClassifier( )
-kfold = KFold(n_splits=num_folds, random_state=seed)
-grid = GridSearchCV(estimator=model, param_grid=param_grid,  
-                                         scoring=scoring, cv=kfold)
-	
-grid_result = grid.fit(X_train, Y_train)
-print("Best:%f using %S" % (grid_result.best_score_,  
-                                        grid_result.best_params_))
-model = GradientBoostingClassifier (max_depth= 5, n_estimators= 180)
-model.fit(X_train, Y_train)
+'''
+n_estimators : int (default=100)
+    The number of boosting stages to perform.
+    Gradient boosting is fairly robust to over-fitting so a large number usually results in better performance.
+max_depth : integer, optional (default=3)
+    maximum depth of the individual regression estimators.
+    The maximum depth limits the number of nodes in the tree.
+    Tune this parameter for best performance; the best value depends on the interaction of the input variables.
 
-# estimate occuracy on validation set
+'''
+n_estimators = [20,180]
+max_depth= [3,5]
+param_grid = dict(n_estimators=n_estimators, max_depth=max_depth)
+model = GradientBoostingClassifier()
+kfold = KFold(n_splits=num_folds, random_state=seed, shuffle=True)
+grid = GridSearchCV(estimator=model, param_grid=param_grid, scoring=scoring, cv=kfold)
+grid_result = grid.fit(X_train, Y_train)
+#Print Results
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+ranks = grid_result.cv_results_['rank_test_score']
+for mean, stdev, param, rank in zip(means, stds, params, ranks):
+    print("#%d %f (%f) with: %r" % (rank, mean, stdev, param))
+
+# prepare model
+model = GradientBoostingClassifier(max_depth= 5, n_estimators= 180)
+model.fit(X_train, Y_train)
+# estimate accuracy on validation set
 predictions = model.predict(X_validation)
-print(accuracy_sore(Y_validation, predictions) )
-print(model.feature_importances_) 
-# use inbuilt class feature_importances
-feat_importances= p.Series(model.feature_importances_, index=X.columns)
-#plot graph of feature importances for better visualization 
-feat_importances.nlargest(10).plot(kind= 'barh')
-pyplot.show( )
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
+
+df_cm = pd.DataFrame(confusion_matrix(Y_validation, predictions), columns=np.unique(Y_validation), index = np.unique(Y_validation))
+df_cm.index.name = 'Actual'
+df_cm.columns.name = 'Predicted'
+sns.heatmap(df_cm, cmap="Blues", annot=True,annot_kws={"size": 16})# font sizes
+
+print(model.feature_importances_) #use inbuilt class feature_importances of tree based classifiers
+#plot graph of feature importances for better visualization
+feat_importances = pd.Series(model.feature_importances_, index=X.columns)
+feat_importances.nlargest(10).plot(kind='barh')
+pyplot.show()
