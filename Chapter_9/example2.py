3bbb11f2b1b8ab26ed5f62be333052cb4861f5d8
MuthuCM <45748076+MuthuCM@users.noreply.github.com>
2025-08-27 23:06:45 +0530

    Update example2.py

--- Chapter_9/example2.py
+++ Chapter_9/example2.py
@@ -9,7 +9,7 @@ X, y = make_moons(n_samples = 100, noise = 0.25, random_state = 0)
 
 # Split into Training and Testing Sets
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X,y, 
+X_train, X_test, y_train, y_test = train_test_split(X,y,
                                     stratify = y,random_state = 1 )
 
 # Fit Random Forest Classifier Model
@@ -26,9 +26,9 @@ print ("F-Score: ", metrics.f1_score(y_test, y_pred, average='weighted'))
 fig, axes = plt.subplots(2, 3, figsize=(20, 10))
 for i, (ax, tree) in enumerate(zip(axes.ravel(),classifier.estimators_)):
   ax.set_title("Tree {}".format(i))
-  mglearn.plots.plot_tree_partition(X_train,y_train,tree, ax = ax)           
-                                                       
-mglearn.plots.plot_2d_separator(classifier, X_train, fill = True, 
+  mglearn.plots.plot_tree_partition(X_train,y_train,tree, ax = ax)
+
+mglearn.plots.plot_2d_separator(classifier, X_train, fill = True,
                                        ax = axes[-1, -1], alpha = 0.4)
 axes[-1, -1].set_title("Random Forest")
 
