c581cc66b40d0ddfabfeb18997d80e2dc8d554b7
MuthuCM <45748076+MuthuCM@users.noreply.github.com>
2025-08-27 23:20:49 +0530

    Update example4.py

--- Chapter_14/example4.py
+++ Chapter_14/example4.py
@@ -31,17 +31,17 @@ plt.show( )
 from sklearn.cluster import KMeans
 kmeans = KMeans(n_clusters = 5, init = 'k-means++', random_state = 0)
 y_kmeans = kmeans.fit_predict(X)
-plt.scatter(X[y_kmeans==0,0], X[y_kmeans==0,1],s=100, c='red', 
+plt.scatter(X[y_kmeans==0,0], X[y_kmeans==0,1],s=100, c='red',
 			label='cluster1')
-plt.scatter(X[y_kmeans==1,0], X[y_kmeans==1,1],s=100, c='blue', 
+plt.scatter(X[y_kmeans==1,0], X[y_kmeans==1,1],s=100, c='blue',
 			label='cluster2')
-plt.scatter(X[y_kmeans==2,0], X[y_kmeans==2,1],s=100, c='green', 
+plt.scatter(X[y_kmeans==2,0], X[y_kmeans==2,1],s=100, c='green',
 			label='cluster3')
-plt.scatter(X[y_kmeans==3,0], X[y_kmeans==3,1],s=100, c='cyan', 
+plt.scatter(X[y_kmeans==3,0], X[y_kmeans==3,1],s=100, c='cyan',
 			label='cluster4')
-plt.scatter(X[y_kmeans==4,0], X[y_kmeans==4,1],s=100, c='magenta', 
+plt.scatter(X[y_kmeans==4,0], X[y_kmeans==4,1],s=100, c='magenta',
 			label='cluster5')
-plt.scatter(kmeans.cluster_centers_[:,0], 				
+plt.scatter(kmeans.cluster_centers_[:,0],
 			kmeans.cluster_centers_[:,1], s=300, c='yellow', label='centroids')
 plt.title('Clusters of Customers')
 plt.xlabel("Annual Income ('000)")
