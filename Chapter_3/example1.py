0a8f1110730f1beb37b99fd935d040f8ea7085e3
MuthuCM <45748076+MuthuCM@users.noreply.github.com>
2025-08-27 22:45:51 +0530

    Update example1.py

--- Chapter_3/example1.py
+++ Chapter_3/example1.py
@@ -1,3 +1,4 @@
+# Example 3.1
 # Import the packages
 import numpy as np
 import pandas as pd
@@ -6,7 +7,6 @@ import matplotlib.pyplot as plt
 # Load the Dataset
 df = pd.read_csv ("insurance.csv")
 df_final = pd.get_dummies (df)
-
 # Define independent variables and dependent variable
 y=df_final ["expenses"].values
 x=df_final.drop ("expenses", axis=1).values
@@ -14,31 +14,27 @@ x=df_final.drop ("expenses", axis=1).values
 # Fitting the Model & Calculating Accuracy
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression( )
-regressor.fit(X,Y)
-y_hat = regressor.predict(X)
+regressor.fit(x,y)
+y_hat = regressor.predict(x)
 print (r2_score(y, y_hat)) #0.75
-
 # Fitting the Model using OLS method
 import statsmodels.formula.api as smf
 results = smf.ols ('expenses ~ age + sex_male + bmi + children +  smoker_yes + region_northwest + region_southeast + region_southwest', data = df_final).fit( )
 print(results.summary)
-
 # Adding Additional Terms
-df_final ['age2'] = df_final.age ** 2	
+df_final ['age2'] = df_final.age ** 2
 df_final ['bmi30'] = (df_final ['bmi'] >=30)*1
 df_final ['bmi30_smoker'] = (df_final['bmi'] >=30)* df_final.smoker_yes
 
 # Defining Independent Variables & Dependent Variable again
 X = df_final [['age', 'age2', 'children', 'bmi', 'sex_male',
 			   'bmi30', 'smoker_yes', 'region_northwest',
-			   'region_southeast', 'region_southwest', 
+			   'region_southeast', 'region_southwest',
 			   'bmi30_smoker']].values
 Y = df_final ["expenses"].values
-
-# Fitting Regression again & Checking Accuracy 
+# Fitting Regression again & Checking Accuracy
 regressor = LinearRegression( )
 regressor.fit (X,Y)
 y_hat = regressor.predict (X)
 
 print(r2_score (Y,y_hat)) # 0.87
-
