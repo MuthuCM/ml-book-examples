f3a5856d90f9b02e2f0a599c0f37f86bd0261ce9
MuthuCM <45748076+MuthuCM@users.noreply.github.com>
2025-08-27 23:05:52 +0530

    Update example1.py

--- Chapter_9/example1.py
+++ Chapter_9/example1.py
@@ -32,8 +32,8 @@ X_test = sc.transform (x_test)
 
 # Fit Random Forest Classifier Model
 from sklearn.ensemble import RandomForestClassifier
-classifier = RandomForestClassifier (n_estimators=10, 
-			                   criterion='entropy', random_state=0) 
+classifier = RandomForestClassifier (n_estimators=10,
+                                           criterion='entropy', random_state=0)
 classifier.fit (X_train,y_train)
 
 # Calculate Accuracy
