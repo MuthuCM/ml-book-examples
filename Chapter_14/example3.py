f2a459ecbf54321480af9677628381b6987675d5
MuthuCM <45748076+MuthuCM@users.noreply.github.com>
2025-08-27 23:19:56 +0530

    Update example3.py

--- Chapter_14/example3.py
+++ Chapter_14/example3.py
@@ -17,15 +17,14 @@ y_kmeans = kmeans.fit_predict(X)
 
 plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s = 100,
                                c = 'red', label = 'Cluster 1')
-plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s = 100, 
+plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s = 100,
                                c = 'blue', label = 'Cluster 2')
 plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s = 100,
                                c = 'green', label = 'Cluster 3')
-plt.scatter(kmeans.cluster_centers_[:,0], kmeans.cluster_centers_ 
+plt.scatter(kmeans.cluster_centers_[:,0], kmeans.cluster_centers_
                [:, 1], s=300, c = 'yellow', label = 'Centroids')
 plt.title('Clusters of Drivers')
 plt.xlabel('Concentration Score (Score1)')
 plt.ylabel('Response Score (Score2)')
 plt.legend( )
-
 plt.show( )
