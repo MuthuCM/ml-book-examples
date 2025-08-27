629a1ef3146a5c6710f865d762b97c04107bd846
MuthuCM <45748076+MuthuCM@users.noreply.github.com>
2025-08-27 22:44:03 +0530

    Update example5.py

--- Chapter_2/example5.py
+++ Chapter_2/example5.py
@@ -13,13 +13,15 @@ Y = df.iloc [:, 1].values
 from sklearn.linear_model import LinearRegression
 lr = LinearRegression( )
 lr.fit(X,Y)
-
 # Visualize the Polynomial Regression relationship
 X_grid = np.arange(min(X), max(X), 0.1)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, Y, color = 'red')
 plt.plot (X_grid, lr_2.predict(pr.fit_transform(X_grid)), color = 'blue')
-plt.title ("Polynomial regression") 
+plt.title ("Polynomial regression")
 plt.xlabel ("Position level")
 plt.ylabel ("Salary")
 plt.show( )
+# Do prediction with Polynomial Regression Model
+
+lr_2.predict(pr.fit_transform([[11]]))
