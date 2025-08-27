6096659afbaa9731731b55265e08b1ec43f2c0bf
MuthuCM <45748076+MuthuCM@users.noreply.github.com>
2025-08-27 23:26:31 +0530

    Update example1.py

--- Chapter_15/example1.py
+++ Chapter_15/example1.py
@@ -6,11 +6,11 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
 from sklearn.feature_selection import SelectKBest
-from sklearn.preprocessing import StandardScaler, LabelEncoder 
-from sklearn.linear_model import LogisticRegression 
-from sklearn.tree import DecisionTreeClassifier 
-from sklearn.ensemble import RandomForestClassifier 
-from sklearn.svm import SVC 
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 
@@ -38,15 +38,15 @@ models = {
    'Random Forest': RandomForestClassifier(random_state=42),
    'SVM': SVC(random_state=42)}
 
-# Function to train and evaluate the model 
+# Function to train and evaluate the model
 def train_and_evaluate(X_train, X_test, y_train, y_test,
-                                               model, method_name): 
-   model.fit(X_train, y_train) 
-   y_pred = model.predict(X_test) 
-   accuracy = accuracy_score(y_test, y_pred) 
-   print(f"{method_name} Accuracy: {accuracy:.2f}") 
-# Loop through each model 
-for model_name, model in models.items(): 
-   print(f"\n{model_name}:\n") 
-   # Classification after PCA 
-   train_and_evaluate(X_train_pca, X_test_pca, y_train, y_test, model, f"{model_name} after PCA")
+                                               model, method_name):
+   model.fit(X_train, y_train)
+   y_pred = model.predict(X_test)
+   accuracy = accuracy_score(y_test, y_pred)
+   print(f"{method_name} Accuracy: {accuracy:.2f}")
+# Loop through each model
+for model_name, model in models.items():
+   print(f"\n{model_name}:\n")
+   # Classification after PCA
+   train_and_evaluate(X_train_pca, X_test_pca, y_train, y_test, model,   f"{model_name} after PCA")
